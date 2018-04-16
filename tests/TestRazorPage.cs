f9d9597688f15c0526059e7727c87b66b8bc3fa2
Erik O'Leary <erik.m.oleary@gmail.com>
2018-04-16 11:55:14 -0500

    Added test project

---
+++ tests/TestRazorPage.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wivuu.RazorInline;
+
+namespace tests
+{
+    [TestClass]
+    public class TestRazorPage
+    {
+        [TestMethod]
+        public async Task TestRendering()
+        {
+            var service = new ServiceCollection()
+                .AddRazorInline()
+                .BuildServiceProvider()
+                .GetRequiredService<IServiceScopeFactory>();
+
+            using (var scope = service.CreateScope())
+            {
+                var renderer = scope.ServiceProvider.GetRequiredService<IViewRenderService>();
+
+                var result = await renderer.RenderToStringAsync("ViewTest", null);
+            }
+        }
+    }
+}
